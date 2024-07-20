492a0d0f343dba8d530be73720cbbe08326826b0
Mahesh-Wijerathna <maheshtharanga49@gmail.com>
2024-07-21 04:25:40 +0530

    page 3

---
+++ Computet-Networks/01 Introduction-Q.js
@@ -0,0 +1,14 @@
+//  2
+//      1) What are the main four sections covered in Introduction Part ?0
+//      2) Three examples for N- edges ?
+//      3) Two examples for N- cores ?
+//  3
+//      4) What are main four part in the Internet ?
+//      5) Other Name for Hosts ?
+//      6) What are running at network edges ?
+//      7) What is done by packet switches ?
+//      8) What is a packet ?
+//      9) Two types of packet switches ?
+//     10) Four Examples for Communication Links ?
+//     11) What is bandwidth ?
+//     12) What is a network ?
